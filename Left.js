41d7799c7bf2ab56aa431955cb203ddc03d2588b
Paddy <paddyfedoo@hotmail.com>
2023-06-20 22:55:45 +0100

    New grid layout and js menu

--- Left.js
+++ Left.js
@@ -1,21 +1,55 @@
-// LEFT MENU
-
-document.write('<img src="men_men.gif" width="150" height="30" border="0" alt="Menu">');
-document.write('<a href="main.html"><img src="men_main.gif" width="150" height="15" border="0" alt="Main"></a>');
-document.write('<a href="member.html"><img src="men_mem.gif" width="150" height="15" border="0" alt="Members"></a>');
-document.write('<a href="join.html"><img src="men_join.gif" width="150" height="15" border="0" alt="Join"></a>');
-document.write('<a href="edit.html"><img src="men_edit.gif" width="150" height="15" border="0" alt="Editorial"></a>');
-document.write('<a href="rteguide.html"><img src="men_rteguide.gif" width="150" height="15" border="0" alt="RTÉ Guide"></a>');
-// document.write('<a href="http://cgi.imperialsenate.co.uk/cgi-bin/forumdisplay.cgi?action=topics&number=15&start=here" target="iswforum"><img src="men_forum.gif" width="150" height="15" border="0" alt="Forum"></a>');
-// document.write('<a href="chat.html" target="iswchat"><img src="men_chat.gif" width="150" height="15" border="0" alt="ISW Chat"></a>');
-document.write('<a href="trivia.html"><img src="men_trivia.gif" width="150" height="15" border="0" alt="Trivia"></a>');
-// document.write('<a href="requests.html"><img src="men_requests.gif" width="150" height="15" border="0" alt="Requests"></a>');
-document.write('<a href="poll.html" target="iswpoll"><img src="men_poll.gif" width="150" height="15" border="0" alt="Poll"></a>');
-document.write('<a href="pollresults.html" target="iswpoll"><img src="men_pollresults.gif" width="150" height="15" border="0" alt="Poll results"></a>');
-// document.write('<a href="posters.html"><img src="men_poster.gif" width="150" height="15" border="0" alt="Poster Store"></a>');
-document.write('<a href="links.html"><img src="men_links.gif" width="150" height="15" border="0" alt="Links"></a>');
-document.write('<a href="linktous.html"><img src="men_linktous.gif" width="150" height="15" border="0" alt="Link to us"></a>');
-document.write('<a href="archive.html"><img src="men_archive.gif" width="150" height="15" border="0" alt="Archives"></a>');
-document.write('<a href="contact.html"><img src="men_contact.gif" width="150" height="15" border="0" alt="Contact me"></a>');
-// document.write('<a href="convention.html"><img src="men_convention.gif" width="150" height="15" border="0" alt="Convention"></a>');
-
+// header
+
+const mainHeader = document.querySelector('header')
+
+const yodaImg = document.createElement('img');
+yodaImg.src="yoda.png";
+yodaImg.width="229";
+yodaImg.height="200";
+yodaImg.alt="Yoda";
+
+const mainImg = document.createElement('img');
+mainImg.src="image2.png";
+mainImg.width="530";
+mainImg.height="200";
+mainImg.alt="ISW";
+
+mainHeader.appendChild(yodaImg);
+mainHeader.appendChild(mainImg);
+
+
+// LEFT MENU
+
+const menu = document.querySelector('#menu');
+
+const menuItems = [
+    { url: 'main.html', linkText: 'Main' },
+    { url: 'member.html', linkText: 'Members' },
+    { url: 'join.html', linkText: 'Join' },
+    { url: 'edit.html', linkText: 'Editorial' },
+    { url: 'rteguide.html', linkText: 'RTE Guide' },
+    { url: 'trivia.html', linkText: 'Trivia' },
+    { url: 'poll.html', linkText: 'Poll' },
+    { url: 'pollresults.html', linkText: 'Poll Results' },
+    { url: 'links.html', linkText: 'Links' },
+    { url: 'linktous.html', linkText: 'Link to Us' },
+    { url: 'archive.html', linkText: 'Archives' },
+    { url: 'contact.html', linkText: 'Contact Me' },
+];
+
+let menuUl = document.createElement('ul');
+
+menuItems.forEach(element => {
+    let a = document.createElement('a');
+    a.href = element.url;
+    a.textContent = element.linkText;
+    let li = document.createElement('li');
+    li.appendChild(a);
+    menuUl.appendChild(li);
+});
+
+const menuHeading = document.createElement('h1');
+menuHeading.textContent = "Menu";
+
+menu.appendChild(menuHeading);
+menu.appendChild(menuUl);
--- quotes.js
+++ quotes.js
@@ -1,43 +1,48 @@
-// quotes
-
-//store the quotations in arrays
-quotes = new Array(34);
-quotes[0] = "May the force be with you.";
-quotes[1] = "No! No different! Only different in your mind.";
-quotes[2] = "Hard to see, the dark side is.";
-quotes[3] = "When nine hundred years old you reach, look as good you will not. Hmm?";
-quotes[4] = "Mudhole? Slimy? My home this is.";
-quotes[5] = "No! Try not. Do. Or do not. There is no try.";
-quotes[6] = "Our own council we will keep on who is ready.";
-quotes[7] = "Your weapons...you will not need them.";
-quotes[8] = "Life creates it, makes it grow. It's energy surrounds us and binds us.";
-quotes[9] = "I sense much fear in you.";
-quotes[10]= "A Jedi's strength flows from the force.";
-quotes[11]="Away with your weapon! I mean you no harm.";
-quotes[12]="Oohhh. Jedi Master. Yoda. You seek Yoda.";
-quotes[13]="Confer on you, the level of Jedi Knight the Council does.";
-quotes[14]="Hmm. That face you make. Look I so old to young eyes?";
-quotes[15]="Help you I can, yes.";
-quotes[16]="A Jedi uses the Force for knowledge and defense, never for attack.";
-quotes[17]="You must feel the Force around you.";
-quotes[18]="A Jedi must have the deepest commitment, the most serious mind.";
-quotes[19]="You must unlearn what you have learned.";
-quotes[20]="Always two there are....no more...no less. A master and an apprentice.";
-quotes[21]="Always with you it cannot be done.";
-quotes[22]="Fear leads to anger, anger leads to hate, hate leads to suffering.";
-quotes[23]="Only a fully trained Jedi Knight with the Force as his ally will conquer.";
-quotes[24]="Not far. Yoda not far. Patience. Soon you will be with him.";
-quotes[25]="Patience! For the Jedi it is time to eat as well.";
-quotes[26]="Through the Force, things you will see. Other places.";
-quotes[27]="I am wondering, why are you here.";
-quotes[28]="Looking? Found someone you have I would say.";
-quotes[29]="Ready, are you? What know you of ready?";
-quotes[30]="Anger...fear...aggression. The dark side of the Force are they.";
-quotes[31]="Fear is the path to the dark side.";
-quotes[32]="For my ally is the Force. And a powerful ally it is.";
-quotes[33]="Hey, dude!";
-//calculate a random index
-index = Math.floor(Math.random() * quotes.length);
-//display the quotation
-document.write("" + "\"" + quotes[index] + "\"\n");
-//done
+//store the quotations in arrays
+
+const quotes = []
+
+quotes.push("May the force be with you.");
+quotes.push("No! No different! Only different in your mind.");
+quotes.push("Hard to see, the dark side is.");
+quotes.push("When nine hundred years old you reach, look as good you will not. Hmm?");
+quotes.push("Mudhole? Slimy? My home this is.");
+quotes.push("No! Try not. Do. Or do not. There is no try.");
+quotes.push("Our own council we will keep on who is ready.");
+quotes.push("Your weapons...you will not need them.");
+quotes.push("Life creates it, makes it grow. It's energy surrounds us and binds us.");
+quotes.push("I sense much fear in you.");
+quotes.push("A Jedi's strength flows from the force.");
+quotes.push("Away with your weapon! I mean you no harm.");
+quotes.push("Oohhh. Jedi Master. Yoda. You seek Yoda.");
+quotes.push("Confer on you, the level of Jedi Knight the Council does.");
+quotes.push("Hmm. That face you make. Look I so old to young eyes?");
+quotes.push("Help you I can, yes.");
+quotes.push("A Jedi uses the Force for knowledge and defense, never for attack.");
+quotes.push("You must feel the Force around you.");
+quotes.push("A Jedi must have the deepest commitment, the most serious mind.");
+quotes.push("You must unlearn what you have learned.");
+quotes.push("Always two there are....no more...no less. A master and an apprentice.");
+quotes.push("Always with you it cannot be done.");
+quotes.push("Fear leads to anger, anger leads to hate, hate leads to suffering.");
+quotes.push("Only a fully trained Jedi Knight with the Force as his ally will conquer.");
+quotes.push("Not far. Yoda not far. Patience. Soon you will be with him.");
+quotes.push("Patience! For the Jedi it is time to eat as well.");
+quotes.push("Through the Force, things you will see. Other places.");
+quotes.push("I am wondering, why are you here.");
+quotes.push("Looking? Found someone you have I would say.");
+quotes.push("Ready, are you? What know you of ready?");
+quotes.push("Anger...fear...aggression. The dark side of the Force are they.");
+quotes.push("Fear is the path to the dark side.");
+quotes.push("For my ally is the Force. And a powerful ally it is.");
+quotes.push("Hey, dude!");
+
+//calculate a random index
+
+const index = Math.floor(Math.random() * quotes.length);
+
+//display the quotation
+
+document.write("" + "\"" + quotes[index] + "\"\n");
+
+//done
